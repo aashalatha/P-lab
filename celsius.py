b0d82be7f3ea872f5fa643f8633aa8230e0a7b8a
Aashalatha P A <93538340+aasha779@users.noreply.github.com>
2021-11-03 11:59:03 +0530

    Add files via upload

---
+++ celsius.py
@@ -0,0 +1,8 @@
+c = int(input("Enter the temperature in degree celsius:"))
+f = (c * 9/5) + 32
+print("Temperature in fahrenheit:",f)
+
+f = int(input("Enter the temperature in fahrenheit :"))
+c = (f - 32) * 5/9 
+print("Temperature in degree celsius:",c)
+
