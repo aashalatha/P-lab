876b6b22a0d19da3fe7edc32e4ea194c4a5330c4
Aashalatha P A <93538340+aashalatha@users.noreply.github.com>
2021-11-24 10:08:15 +0530

    Delete celsius.py

--- celsius.py
+++
@@ -1,8 +0,0 @@
-c = int(input("Enter the temperature in degree celsius:"))
-f = (c * 9/5) + 32
-print("Temperature in fahrenheit:",f)
-
-f = int(input("Enter the temperature in fahrenheit :"))
-c = (f - 32) * 5/9 
-print("Temperature in degree celsius:",c)
-
