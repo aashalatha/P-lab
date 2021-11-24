4054eb725ec5acd03322bd6ef7dd8ccac5b84b7e
Aashalatha P A <93538340+aashalatha@users.noreply.github.com>
2021-11-24 11:20:28 +0530

    Add files via upload

---
+++ lab cycle 3/experiment 23.py
@@ -0,0 +1,8 @@
+def number(num):
+    for i in range(1,num+1):
+        for j in range(1,i+1):
+            print(i*j,end=" ")
+        print("\n")
+        
+n = int(input("Enter the number:"))
+print(number(n))
