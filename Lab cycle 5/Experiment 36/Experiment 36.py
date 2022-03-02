376a8f535f976bfdec49914b4ed080a4cd1734e6
Aashalatha P A <93538340+aashalatha@users.noreply.github.com>
2022-03-02 22:19:15 +0530

    Delete Lab cycle 5 directory

--- Lab cycle 5/Experiment 36/Experiment 36.py
+++
@@ -1,8 +0,0 @@
-import csv
-with open("username.csv", "r") as csvfile:
- data = csv.DictReader(csvfile, delimiter=";")
- 
- print("Identifier Username")
- print("---------------------------------")
- for row in data:
-   print(row[' Identifier'], row['Username'])
